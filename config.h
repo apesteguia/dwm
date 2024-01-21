76b706d5ef7551240877de765429edeee4911fc0
apesteguia <mikeltxomikeltxo@gmail.com>
2024-01-22 00:10:59 +0100

    dmenu

--- config.h
+++ config.h
@@ -77,12 +77,13 @@ static const Layout layouts[] = {
     }
 
 /* commands */
+static const char d_lines[] = "20";
 static char dmenumon[2]
     = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]
-    = { "dmenu_run", "-m",      dmenumon,  "-fn",     dmenufont,
-        "-nb",       col_gray1, "-nf",     col_gray3, "-sb",
-        col_cyan,    "-sf",     col_gray4, NULL };
+    = { "dmenu_run", "-m",  dmenumon,  "-fn", dmenufont, "-nb",
+        col_gray1,   "-nf", col_gray3, "-sb", col_cyan,  "-sf",
+        col_gray4,   "-l",  d_lines,   "-c",  NULL };
 static const char *termcmd[] = { "alacritty", NULL };
 static const char *browser[] = { "brave-browser", NULL };
 static const char *thunar[] = { "thunar", NULL };
